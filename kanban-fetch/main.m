222f0e0efcd730f8be3295917f4366c07fc8cde8
Jan-Yves Ruzicka <jan@1klb.com>
2013-11-18 21:54:34 +1300

    Initial Commit

---
+++ kanban-fetch/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  kanban-fetch
+//
+//  Created by Jan-Yves on 18/11/13.
+//  Copyright (c) 2013 Jan-Yves Ruzicka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
